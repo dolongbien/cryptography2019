989d2ce13a0bb726714ec2c9531d35f9ae9005b8
To Hoai Bao <tohoaibao@gmail.com>
2019-03-22 16:44:38 +0700

    Add RSA

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QDialogButtonBox
 from PyQt5.QtCore import QDateTime, Qt, QTimer
+from Crypto.PublicKey import RSA
 
 # Import the compiled UI module
 # these modules are auto generated from .ui files designed in Qt Designer
+from rsa_ui import Ui_RSADialog
 from window_ui import Ui_MainWindow
 from key_ui import Ui_KeyDialog
 import config
@@ -87,25 +89,34 @@ class Main(QMainWindow):
                     item = listWidget.item(i).text()
                     tar.add(item, split(item)[1])
             # Getting password(key) for encryption
-            pass_dialog = PasswordDlg(self)
-            if pass_dialog.exec_():
-                password = pass_dialog.password()
-                cipher = AllCipher(str(password), save_file)
-                start = time.time()
-                # Encrypt ratio option: AES, DES, RSA
-                if self.ui.aes.isChecked():
-                    if cipher.encrypt_AES():
+            if self.ui.rsa.isChecked():
+                rsa_dialog = RSADlg(self)
+                rsa_dialog.changeLabel("Public key: ")
+                if rsa_dialog.exec_():
+                    public_key = rsa_dialog.path()
+                    cipher = AllCipher(str(public_key), save_file, isRSA=True)
+                    start = time.time()
+                    if cipher.encrypt_RSA():
                         end = time.time()
                         self.message_box(str(end - start))
                         os.remove(tempfile)
-                elif self.ui.des.isChecked():
-                    if cipher.encrypt_DES():
-                        end = time.time()
-                        self.message_box(str(end - start))
-                        os.remove(tempfile)
-                elif self.ui.rsa.isChecked():
-                    pass
-                    # TODO
+            else:
+                pass_dialog = PasswordDlg(self)
+                if pass_dialog.exec_():
+                    password = pass_dialog.password()
+                    cipher = AllCipher(str(password), save_file)
+                    start = time.time()
+                    # Encrypt ratio option: AES, DES
+                    if self.ui.aes.isChecked():
+                        if cipher.encrypt_AES():
+                            end = time.time()
+                            self.message_box(str(end - start))
+                            os.remove(tempfile)
+                    elif self.ui.des.isChecked():
+                        if cipher.encrypt_DES():
+                            end = time.time()
+                            self.message_box(str(end - start))
+                            os.remove(tempfile)
 
     def browse(self):
         self.encrypted_file = QFileDialog.getOpenFileName()[0]
@@ -118,30 +129,36 @@ class Main(QMainWindow):
             self.ui.decryptButton.setEnabled(False)
 
     def decrypt(self):
-
-        pass_dialog = PasswordDlg(self)
         infile = self.ui.lineEdit.text()
         if not os.path.isfile(infile):
             self.message_box("File not found!")
             return
-        if pass_dialog.exec_():
-            password = pass_dialog.password()
-            cipher = AllCipher(password, infile)
-            outfile = False
-            if self.ui.aes.isChecked():
-                outfile = cipher.decrypt_AES()  
-            elif self.ui.des.isChecked():
-                outfile = cipher.decrypt_DES()
-            elif self.ui.rsa.isChecked():
-                pass
-            if outfile:
-                dir, fname = split(outfile)
-                prefix = fname.rsplit('.tar', 1)[0]+' '
-                outdir = tempfile.mkdtemp(prefix=prefix, dir=dir)
-                with tarfile.open(outfile, 'r') as tar:
-                    tar.extractall(outdir)
-                os.remove(outfile)
-                self.message_box("Successful Decryption!")
+
+        outfile = False
+        if self.ui.rsa.isChecked():
+            rsa_dialog = RSADlg(self)
+            rsa_dialog.changeLabel("Private key: ")
+            if rsa_dialog.exec_():
+                private_key = rsa_dialog.path()
+                cipher = AllCipher(private_key, infile, isRSA=True)
+                outfile = cipher.decrypt_RSA()
+        else:
+            pass_dialog = PasswordDlg(self)
+            if pass_dialog.exec_():
+                password = pass_dialog.password()
+                cipher = AllCipher(password, infile)
+                if self.ui.aes.isChecked():
+                    outfile = cipher.decrypt_AES()
+                elif self.ui.des.isChecked():
+                    outfile = cipher.decrypt_DES()
+        if outfile:
+            dir, fname = split(outfile)
+            prefix = fname.rsplit('.tar', 1)[0]+' '
+            outdir = tempfile.mkdtemp(prefix=prefix, dir=dir)
+            with tarfile.open(outfile, 'r') as tar:
+                tar.extractall(outdir)
+            os.remove(outfile)
+            self.message_box("Successful Decryption!")
 
     def message_box(self, text, type=BOXTYPE.SUCCESS):
         msgBox = QMessageBox()
@@ -179,6 +196,51 @@ class PasswordDlg(QDialog):
 
     def show_password(self):
         self.ui.lineEdit.setEchoMode(QLineEdit.Normal)
+
+class RSADlg(QDialog):
+
+    def __init__(self, parent=None, master=False):
+        QDialog.__init__(self, parent=parent)
+
+        # This is always the same
+        self.ui = Ui_RSADialog()
+        self.ui.setupUi(self)
+
+        ok_button = self.ui.buttonBox.button(QDialogButtonBox.Ok)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/icon/image/check.png"),
+                       QtGui.QIcon.Normal,
+                       QtGui.QIcon.Off)
+        ok_button.setIcon(icon)
+
+    def path(self):
+        return self.ui.lineEdit.text()
+
+    def changeLabel(self, content):
+        self.ui.label.setText(content)
+
+    def browse(self):
+        self.key = QFileDialog.getOpenFileName()[0]
+        self.ui.lineEdit.setText(self.key)
+
+    def generate_key_RSA(self):
+        filter = 'RSA Key (*.pem)'
+        public_key = QFileDialog.getSaveFileName(self,
+                                             'Save public key',
+                                             filter=filter)[0]
+        if public_key:
+            private_key = QFileDialog.getSaveFileName(self,
+                                                 'Save private key',
+                                                 filter=filter)[0]
+            if private_key:
+                #Generate a public/ private key pair using 4096 bits key length (512 bytes)
+                key = RSA.generate(4096)
+                with open(public_key, "wb") as public:
+                    with open(private_key, "wb") as private:
+                        public.write(key.publickey().export_key("PEM"))
+                        private.write(key.export_key("PEM"))
+                        public.close()
+                        private.close()
 
 
 class ProgressDlg(QDialog):
