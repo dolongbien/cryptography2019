1b454f9a2099119d8730a572fb5ae99632a388a7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 13:32:56 +0000

    Adding tarfile member sanitization to extractall()

--- main.py
+++ main.py
@@ -168,7 +168,26 @@ class Main(QMainWindow):
             prefix = fname.rsplit('.tar', 1)[0]+' '
             outdir = tempfile.mkdtemp(prefix=prefix, dir=dir)
             with tarfile.open(outfile, 'r') as tar:
-                tar.extractall(outdir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, outdir)
             os.remove(outfile)
             end = time.time()
             self.message_box("Successful Decryption In " + str(round((end - start),3)) + ' s')
